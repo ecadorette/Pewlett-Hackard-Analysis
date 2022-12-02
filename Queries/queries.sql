3d39f916b3e572dd992c6b5e2a59b5cbafb31edd
Elizabeth Cadorette <liz.cadorette@gmail.com>
2022-12-01 23:03:44 -0500

    Progress Upload

---
+++ Queries/queries.sql
@@ -0,0 +1,18 @@
+SELECT employees.emp_no, 
+	employees.first_name, 
+	employees.last_name,
+	titles.title,
+	titles.from_date,
+	titles.to_date
+INTO retirement_titles	
+FROM employees INNER JOIN titles ON employees.emp_no = titles.emp_no
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY emp_no;
+
+
+SELECT DISTINCT ON (emp_no) r.emp_no,r.first_name,r.last_name,r.title
+
+--INTO unique_titles
+FROM retirement_titles as r
+WHERE to_date='9999-01-01'
+ORDER BY emp_no ASC, to_date DESC;
--- Queries/schema.sql
+++ Queries/schema.sql
@@ -11,3 +11,29 @@ FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 );
 
 SELECT * FROM departments;
+
+--did not save the original table scripts, kept re-using what I already had
+
+SELECT * FROM departments;
+
+DROP TABLE employees CASCADE;
+
+CREATE TABLE employees(
+	emp_no INT NOT NULL,
+	birth_date date NOT NULL,
+	first_name VARCHAR(40) NOT NULL,
+	last_name VARCHAR(40) NOT NULL,
+	gender VARCHAR(1) NOT NULL,
+	hire_date date NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+DROP TABLE titles;
+
+CREATE TABLE titles(
+	emp_no INT NOT NULL,
+	title VARCHAR(40),
+	from_date date,
+	to_date date,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
